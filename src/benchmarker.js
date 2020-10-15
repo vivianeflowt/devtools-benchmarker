81be4e4a7f31c655ee82e81966888cfe0f5f0f00
vivianeflowt <vivianeflowt@gmail.com>
2020-10-15 08:54:24 -0300

    new feature avg

--- src/benchmarker.js
+++ src/benchmarker.js
@@ -7,31 +7,44 @@
 'use strict'
 
 var SETTINGS = {
-    PRECISION: 6,
+  PRECISION: 6,
 }
 
 const setPrecision = function (value = 6) {
-    if (value >= 1) {
-        SETTINGS.PRECISION = value
-    }
+  if (value >= 1) {
+    SETTINGS.PRECISION = value
+  }
 }
 
 const getPrecision = function () {
-    return SETTINGS.PRECISION
+  return SETTINGS.PRECISION
 }
 
 const measure = function (callback = Function()) {
-    const startAt = process.hrtime()
-    callback()
-    const diff = process.hrtime(startAt)
-    const deltatime = parseFloat(
-        (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(SETTINGS.PRECISION)
-    )
-    return deltatime
+  const startAt = process.hrtime()
+  callback()
+  const diff = process.hrtime(startAt)
+  const deltatime = parseFloat(
+    (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(SETTINGS.PRECISION)
+  )
+  return deltatime
+}
+
+const measureAvg = function (callback = Function(), execution = 100) {
+  var measureList = []
+  for (let index = 0; index < execution + 1; index++) {
+    measureList.push(measure(callback))
+  }
+  measureList.shift()
+  const sum = measureList.reduce(function (acc, value) {
+    return acc + value;
+  }, 0)
+  return parseFloat((sum / execution).toFixed(SETTINGS.PRECISION))
 }
 
 module.exports = {
-    getPrecision,
-    setPrecision,
-    measure,
+  getPrecision,
+  setPrecision,
+  measure,
+  measureAvg,
 }
