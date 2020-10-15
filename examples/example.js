43f61d1fefcf3aeffe1ebd5f3258398df34b4f16
vivianeflowt <vivianeflowt@gmail.com>
2020-10-15 08:16:51 -0300

    new release

--- examples/example.js
+++ examples/example.js
@@ -2,12 +2,12 @@ const benchmarker = require('../index')
 
 // Function to Test
 const somefunction = function () {
-    // Some Complex Things
-    for (let i = 0; i < 100; i++) {
-        for (let j = 0; j < 100; j++) {
-            var someComplexCalc = Math.random * 1000 + Math.random * 1000
-        }
+  // Some Complex Things
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      var someComplexCalc = Math.random * 1000 + Math.random * 1000
     }
+  }
 }
 
 // set precision of measured value
